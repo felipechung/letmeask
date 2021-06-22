3f07bdca3de2e956f7b5ba2efcc9f72d2009a6eb
Felipe Chung <fchung.dev@gmail.com>
2021-06-22 18:30:27 -0300

    create button and newroom page

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -5,8 +5,7 @@ import googleIcon from '../assets/images/google-icon.svg'
 import '../styles/auth.scss'
 import {Button} from '../components/Button'
 
-
-export function Home () {
+export function NewRoom () {
     return (
         <div id="page-auth">
             <aside>
@@ -18,17 +17,15 @@ export function Home () {
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                 
-                <button className="create-room">
-                    <img src={googleIcon} alt="Logo do google" />
-                    Crie sua sala com o google
-                </button>
-                <div className="separator">ou entre em uma sala</div>
+                    <h2>Criar uma nova sala</h2>
+                
                 <form action="">
                     <input type="text"
-                            placeholder="Digite o código da sala"
+                            placeholder="Nome da sala"
                     />
-                    <Button type="submit">Entrar na sala</Button>
+                    <Button type="submit">Criar sala</Button>
                 </form>
+                <p>Quer entrar em uma sala existente? <a href="#">clique aqui</a></p>
                 </div>
             </main>
         </div>
