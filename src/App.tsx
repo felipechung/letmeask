3aabe5e199c0ee52cddb07aae1267793b295870c
Felipe Chung <fchung.dev@gmail.com>
2021-06-24 19:01:29 -0300

    create admin room features

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {AuthContextProvider} from "./contexts/AuthContext"
 import {Home} from "./pages/Home"
 import {NewRoom} from "./pages/NewRoom"
 import {Room} from "./pages/Room"
+import {AdminRoom} from "./pages/AdminRoom"
 
 
 
@@ -22,6 +23,7 @@ function App() {
         <Route path="/" exact component={Home}></Route>
         <Route path="/rooms/new" component={NewRoom}></Route>     
         <Route path="/rooms/:id" component={Room}></Route>
+        <Route path="/admin/rooms/:id"component={AdminRoom}></Route>
       </Switch>
     </BrowserRouter>
     </AuthContextProvider>
